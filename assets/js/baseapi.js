2a312e70510cee97a6c3f18969d9718b0b8d6afa
jenny <2279823852@qq.com>
2022-05-21 16:20:37 +0800

    完成了主页功能开发

--- assets/js/baseapi.js
+++ assets/js/baseapi.js
@@ -2,4 +2,19 @@
 $.ajaxPrefilter(function(options) {
     console.log(options.url);
     options.url = 'http://www.liulongbin.top:3007' + options.url;
+    // 统一为有权限的接口，设置headers请求头
+    if (options.url.indexOf('/my/') !== 1)
+        options.headers = { Authorization: localStorage.getItem('token') || '' }
+        // 全局统一挂载 complete 回调函数
+    options.complete = function(res) {
+        // console.log('complete');
+        // console.log(res);
+        // complete中 可以使用res.responseJSON 拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1) {
+            // 1 强制清空token
+            localStorage.removeItem('token');
+            // 2 强制跳转到登录页面
+            location.href = 'login.html';
+        }
+    }
 })
